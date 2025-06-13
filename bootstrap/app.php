debf16609bfaa1bc3730ef78f630bb6ccedd86e4
azwaril <hasibiazwaril@gmail.com>
2025-06-13 13:52:21 +0700

    done banget

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'admin'       => IsAdmin::class,       // Middleware custom admin kamu
+            'admin'       => IsAdmin::class,       
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
